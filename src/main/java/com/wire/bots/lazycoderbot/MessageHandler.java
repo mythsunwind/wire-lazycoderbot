ca385fc8753ef1e5d6ee9bfd4db3166cb4eb09c4
Sven Jost <sven@wire.com>
2017-02-17 10:57:27 +0100

    Sort languages by alphabet and add sql

--- src/main/java/com/wire/bots/lazycoderbot/MessageHandler.java
+++ src/main/java/com/wire/bots/lazycoderbot/MessageHandler.java
@@ -172,7 +172,7 @@ public class MessageHandler extends MessageHandlerBase {
     }
 
     private String getLanguage(String message) {
-        String supportedLanguages = "(java|python|javascript|swift|perl|html|css|c\\+\\+|c#|bash|scala|haskell|prolog|rust)";
+        String supportedLanguages = "(c|c\\+\\+|c#|css|bash|haskell|html|java|javascript|perl|prolog|python|rust|scala|sql|swift)";
         Pattern pattern = Pattern.compile(String.format("^%s .*$", supportedLanguages));
         Matcher m = pattern.matcher(message);
         if (m.find()) {
